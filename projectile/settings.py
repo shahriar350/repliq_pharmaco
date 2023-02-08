d8f201d132e0584770ff0b2d0f7653bdfa4122aa
Saifullah Shahen <44544213+shahriar350@users.noreply.github.com>
2023-02-08 18:50:28 +0600

    Update settings.py

--- projectile/settings.py
+++ projectile/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-1(k)dd710l%!w&r=1@5kl%c_1r97bun#*mcnk-y2l^%s%!gh+4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 DJANGO_APPS = [
